2c6eddc77d16ed046938c56894020d258bb6c83c
BatonxlebaK <125285391+BatonxlebaK@users.noreply.github.com>
2023-06-07 19:57:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//inputs
+let input1 = document.querySelector("#num1");
+let num1 = input1.value;
+let input2 = document.querySelector("#num2");
+let num2 = input2.value;
+let resultInput = document.querySelector("#result");
+let result = resultInput.value;
+
+
+//buttons
+let buttonAdd = document.querySelector("#add");
+let buttonSubtraction = document.querySelector("#subtraction");
+let buttonMultiply = document.querySelector("#multiply");
+let buttonDivision = document.querySelector("#division");
+
+
+
+//scripts
+buttonAdd.addEventListener("onclick", () => resultInput.innerHTML = num1 + num2);
+buttonSubtraction.addEventListener("onclick", () => num1 - num2);
+buttonMultiply.addEventListener("onclick", () => num1 * num2);
+buttonDivision.addEventListener("onclick", () => num1 / num2);
